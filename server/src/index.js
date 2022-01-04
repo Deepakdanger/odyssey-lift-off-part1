56bd30ce7e2ffbced2bfa3980054504edfd9f87c
Deepakdanger <depk.96.kumar@gmail.com>
2022-01-04 20:55:53 +0530

    Add apolloserver

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
 //TODO
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks: true
+  });
+
+server.listen().then(() => {
+    console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+  });
